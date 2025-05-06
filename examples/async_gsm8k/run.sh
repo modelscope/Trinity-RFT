3fc6b2e69cd8b01366d8d371ba5a5e1e9828b8d1
yuchang <sycgloria@gmail.com>
2025-05-06 17:49:15 +0800

    add running command for async

--- examples/async_gsm8k/run.sh
+++ examples/async_gsm8k/run.sh
@@ -1,7 +1,4 @@
-# trinity run --config examples/async_gsm8k/explorer.yaml 2>&1 | tee explorer.log &
-# sleep 30
-# trinity run --config examples/async_gsm8k/trainer.yaml 2>&1 | tee trainer.log &
-
-python -m trinity.cli.launcher run --config examples/async_gsm8k/explorer.yaml 2>&1 | tee explorer.log &
+#!/bin/bash
+trinity run --config examples/async_gsm8k/explorer.yaml 2>&1 | tee explorer.log &
 sleep 30
-python -m trinity.cli.launcher run --config examples/async_gsm8k/trainer.yaml 2>&1 | tee trainer.log &
+trinity run --config examples/async_gsm8k/trainer.yaml 2>&1 | tee trainer.log &
