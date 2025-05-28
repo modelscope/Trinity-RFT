f8088b4cf95c5dd31f08c28bb1e31cb03462147a
garyzhang99 <garyzhang99@163.com>
2025-05-28 16:14:58 +0800

    add workflows to init

--- trinity/common/workflows/__init__.py
+++ trinity/common/workflows/__init__.py
@@ -4,12 +4,13 @@ from .envs.alfworld.alfworld_workflow import AlfworldWorkflow
 from .envs.sciworld.sciworld_workflow import SciWorldWorkflow
 from .envs.webshop.webshop_workflow import WebShopWorkflow
 from .math_workflows import MathBasedModelWorkflow, MathWorkflow
-from .workflow import WORKFLOWS, SimpleWorkflow, Task
+from .workflow import WORKFLOWS, SimpleWorkflow, Task, BaseModelWorkflow
 
 __all__ = [
     "Task",
     "WORKFLOWS",
     "SimpleWorkflow",
+    "BaseModelWorkflow",
     "MathWorkflow",
     "MathBasedModelWorkflow",
     "WebShopWorkflow",
