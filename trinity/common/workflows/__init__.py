d0c64a35bfea1aa34a268d19b89b70fe317429fd
garyzhang99 <garyzhang99@163.com>
2025-05-28 16:15:22 +0800

    pass pre-commit

--- trinity/common/workflows/__init__.py
+++ trinity/common/workflows/__init__.py
@@ -4,7 +4,7 @@ from .envs.alfworld.alfworld_workflow import AlfworldWorkflow
 from .envs.sciworld.sciworld_workflow import SciWorldWorkflow
 from .envs.webshop.webshop_workflow import WebShopWorkflow
 from .math_workflows import MathBasedModelWorkflow, MathWorkflow
-from .workflow import WORKFLOWS, SimpleWorkflow, Task, BaseModelWorkflow
+from .workflow import WORKFLOWS, BaseModelWorkflow, SimpleWorkflow, Task
 
 __all__ = [
     "Task",
