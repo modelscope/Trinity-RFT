b5d3e096dd8a0fbeefbd9e574f00823259579699
weijie <shiweijie0311@foxmail.com>
2025-06-11 16:43:33 +0800

    1. Init BasicEntropyLossFn default_args entropy_coef using zero, otherwise, check_and_update will be error. 2. fix writting typo in programming_guide

--- trinity/algorithm/entropy_loss_fn/entropy_loss_fn.py
+++ trinity/algorithm/entropy_loss_fn/entropy_loss_fn.py
@@ -16,10 +16,10 @@ class EntropyLossFn(ABC):
 
     @abstractmethod
     def __call__(
-        self,
-        entropy: torch.Tensor,
-        action_mask: torch.Tensor,
-        **kwargs,
+            self,
+            entropy: torch.Tensor,
+            action_mask: torch.Tensor,
+            **kwargs,
     ) -> Tuple[torch.Tensor, Dict]:
         """
         Args:
@@ -32,7 +32,6 @@ class EntropyLossFn(ABC):
         """
 
     @classmethod
-    @abstractmethod
     def default_args(cls) -> Dict:
         """
         Returns:
@@ -51,17 +50,13 @@ class BasicEntropyLossFn(EntropyLossFn):
         self.entropy_coef = entropy_coef
 
     def __call__(
-        self,
-        entropy: torch.Tensor,
-        action_mask: torch.Tensor,
-        **kwargs,
+            self,
+            entropy: torch.Tensor,
+            action_mask: torch.Tensor,
+            **kwargs,
     ) -> Tuple[torch.Tensor, Dict]:
         entropy_loss = masked_mean(entropy, action_mask)
         return entropy_loss * self.entropy_coef, {"entropy_loss": entropy_loss.detach().item()}
-
-    @classmethod
-    def default_args(cls) -> Dict:
-        return {"entropy_coef": 0.0}
 
 
 @ENTROPY_LOSS_FN.register_module("none")
@@ -74,13 +69,9 @@ class DummyEntropyLossFn(EntropyLossFn):
         pass
 
     def __call__(
-        self,
-        entropy: torch.Tensor,
-        action_mask: torch.Tensor,
-        **kwargs,
+            self,
+            entropy: torch.Tensor,
+            action_mask: torch.Tensor,
+            **kwargs,
     ) -> Tuple[torch.Tensor, Dict]:
         return torch.tensor(0.0), {}
-
-    @classmethod
-    def default_args(cls) -> Dict:
-        return {}
