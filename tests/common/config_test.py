0d1d53175b8f3003081640839c5381de7d4f0cd4
hiyuchang <sycgloria@gmail.com>
2025-07-17 17:56:15 +0800

    fix unittest

--- tests/common/config_test.py
+++ tests/common/config_test.py
@@ -58,22 +58,26 @@ class TestConfig(unittest.TestCase):
                         raise e
 
     def test_continue_from_checkpoint_is_valid(self):
-        config = get_template_config()
-        config.name = "test"
-        config.project = "unittest"
-        config.checkpoint_root_dir = "./temp_checkpoint_dir"
+        self.config = get_template_config()
+        self.config.name = "test"
+        self.config.project = "unittest"
+        self.config.checkpoint_root_dir = "./temp_checkpoint_dir"
 
-        dir_path = os.path.join(config.checkpoint_root_dir, config.project, config.name)
+        dir_path = os.path.join(
+            self.config.checkpoint_root_dir, self.config.project, self.config.name
+        )
         os.makedirs(os.path.join(dir_path, "global_step_1"))
 
-        config.continue_from_checkpoint = True
-        config.check_and_update()
-        self.assertEqual(config.name, "test")
+        self.config.continue_from_checkpoint = True
+        self.config.check_and_update()
+        self.assertEqual(self.config.name, "test")
 
-        config.continue_from_checkpoint = False
-        config.check_and_update()
-        self.assertTrue(config.name.startswith("test_"))
-        timestamp = config.name.split("_")[-1]
+        self.config.continue_from_checkpoint = False
+        self.config.check_and_update()
+        self.assertTrue(self.config.name.startswith("test_"))
+        timestamp = self.config.name.split("_")[-1]
         self.assertTrue(datetime.datetime.strptime(timestamp, "%Y%m%d%H%M%S"))
 
-        shutil.rmtree(config.checkpoint_root_dir)
+    def tearDown(self):
+        # remove dir only when the test passed
+        shutil.rmtree(self.config.checkpoint_root_dir)
