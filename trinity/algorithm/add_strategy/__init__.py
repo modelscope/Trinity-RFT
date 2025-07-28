66f92b291750cfaaaaf1ae71fbd58a48a0dd9954
hiyuchang <sycgloria@gmail.com>
2025-07-28 19:29:03 +0800

    add new strategy

--- trinity/algorithm/add_strategy/__init__.py
+++ trinity/algorithm/add_strategy/__init__.py
@@ -1,11 +1,13 @@
 from trinity.algorithm.add_strategy.add_strategy import (
     ADD_STRATEGY,
     AddStrategy,
+    DuplicateInformativeAddStrategy,
     RewardVarianceAddStrategy,
 )
 
 __all__ = [
     "ADD_STRATEGY",
     "AddStrategy",
+    "DuplicateInformativeAddStrategy",
     "RewardVarianceAddStrategy",
 ]
--- trinity/algorithm/add_strategy/add_strategy.py
+++ trinity/algorithm/add_strategy/add_strategy.py
@@ -64,6 +64,64 @@ class RewardVarianceAddStrategy(AddStrategy):
         return {"variance_threshold": 0.0}
 
 
+@ADD_STRATEGY.register_module("duplicate_informative")
+class DuplicateInformativeAddStrategy(AddStrategy):
+    """An example AddStrategy that only adds experiences with non-zero advantage and repeats them to reach the target size
+    Ref: POLARIS
+    """
+
+    def __init__(self, writer: BufferWriter, variance_threshold: float = 0.0, **kwargs) -> None:
+        super().__init__(writer)
+        self.variance_threshold = variance_threshold
+
+    async def add(self, experiences: List[Experience], step: int) -> int:
+        cnt = 0
+        cnt_tot = len(experiences)
+        effective_tasks, effective_experiences = [], []
+        grouped_experiences = group_by(experiences, id_type="task")
+        for task_id, group_exps in grouped_experiences.items():
+            if len(group_exps) < 2:
+                continue
+            # check if the rewards are the same
+            rewards = [exp.reward for exp in group_exps]
+            variance = np.var(rewards)
+            if variance <= self.variance_threshold:
+                continue
+            cnt += len(group_exps)
+            effective_tasks.append(task_id)
+            effective_experiences.extend(group_exps)
+
+        if not effective_tasks:
+            return 0
+
+        import copy
+        import random
+
+        task_ids_to_add = effective_tasks.copy()
+        task_id_offset = len(grouped_experiences)
+        while cnt < cnt_tot:
+            if not task_ids_to_add:
+                task_ids_to_add = effective_tasks.copy()
+                task_id_offset += len(grouped_experiences)
+            task_id = random.choice(task_ids_to_add)
+            task_ids_to_add.remove(task_id)
+
+            copied_exps = copy.deepcopy(grouped_experiences[task_id])
+
+            for exp in copied_exps:
+                exp.eid.task += task_id_offset
+
+            cnt += len(copied_exps)
+            effective_experiences.extend(copied_exps)
+
+        await self.writer.write_async(effective_experiences)
+        return cnt
+
+    @classmethod
+    def default_args(cls) -> dict:
+        return {"variance_threshold": 0.0}
+
+
 def group_by(
     experiences: List[Experience], id_type: Literal["task", "run", "step"]
 ) -> Dict[str, List[Experience]]:
