227a4ff5eca3bb3a867e9ef3feb9bddcda51e0ac
hiyuchang <sycgloria@gmail.com>
2025-07-30 09:24:42 +0800

    add correct_bias_strategy

---
+++ trinity/algorithm/add_strategy/correct_bias_strategy.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""An Addstrategy with GroupAdvantage that corrects for rank bias (https://arxiv.org/pdf/2506.02355)"""
+from typing import Dict, List, Tuple
+
+import torch
+import numpy as np
+
+from trinity.algorithm.add_strategy import ADD_STRATEGY, GRPOAddStrategy
+from trinity.algorithm.utils import masked_mean
+from trinity.buffer import BufferWriter
+from trinity.common.experience import Experience
+
+
+@ADD_STRATEGY.register_module("correct_bias")
+class CorrectBiasAddStrategy(GRPOAddStrategy):
+    """An example AddStrategy with advantages that corrects for rank bias"""
+
+    def __init__(self, writer: BufferWriter, epsilon: float = 1e-6, rank_penalty: float = 0.25, **kwargs) -> None:
+        super().__init__(writer)
+        self.epsilon = epsilon
+        self.rank_penalty = rank_penalty
+    def calculate_group_advantage(
+        self, group_id: str, exps: List[Experience]
+    ) -> Tuple[List[Experience], Dict]:
+        with torch.no_grad():
+            if len(exps) == 1:
+                group_reward_mean = torch.tensor(0.0)
+                group_reward_std = torch.tensor(1.0)
+            else:
+                rewards = torch.tensor([exp.reward for exp in exps])
+                corrected_rewards = rewards.clone()
+
+                # correct bias
+                old_log_probs = torch.tensor([torch.mean(exp.logprobs, axis=-1) for exp in exps]) 
+                group_ranks = torch.argsort(torch.argsort(old_log_probs))
+                corrected_rewards = corrected_rewards * (1 - group_ranks * self.rank_penalty)
+
+                group_reward_mean = torch.mean(corrected_rewards)
+                group_reward_std = torch.std(corrected_rewards)
+                
+            for i, exp in enumerate(exps):
+                score = (corrected_rewards[i] - group_reward_mean) / (group_reward_std + self.epsilon)
+                exp.advantages = score * exp.action_mask
+                exp.returns = exp.advantages.clone()
+
+            metrics = {
+                "reward_mean": group_reward_mean.item(),
+                "reward_std": group_reward_std.item(),
+            }
+
+        return exps, metrics
+
+    @classmethod
+    def default_args(cls) -> dict:
+        return {"epsilon": 1e-6,
+                "rank_penalty": 0.25}
