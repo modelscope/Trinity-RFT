51707ffa3b5e9298a4d8fe653008058a769ff4b7
chenyushuo <297086016@qq.com>
2025-08-12 15:59:52 +0800

    Bug fix in dlc mode && apply suggestions form gemini

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -3,7 +3,12 @@ import os
 import subprocess
 import time
 
+import torch
+import torch.distributed as dist
 import yaml
+
+from trinity.algorithm.algorithm import ALGORITHM_TYPE
+from trinity.utils.dlc_utils import get_dlc_env_vars
 
 
 def set_engine_num(config, args):
@@ -21,56 +26,77 @@ def set_engine_num(config, args):
         config["explorer"]["rollout_model"]["engine_num"] = args.vllm_engine_num
     else:  # auto set engine_num
         opt_explorer_num, opt_ratio_diff = None, float("inf")
+        total_gpu_num = args.node_num * args.gpu_per_node
+
+        def update_opt_explorer_num(trainer_gpu_num, opt_explorer_num, opt_ratio_diff):
+            if batch_size % trainer_gpu_num != 0:
+                return opt_explorer_num, opt_ratio_diff
+            explorer_gpu_num = total_gpu_num - trainer_gpu_num
+            if explorer_gpu_num % tensor_parallel_size != 0:
+                return opt_explorer_num, opt_ratio_diff
+            explorer_num = explorer_gpu_num // tensor_parallel_size
+            ratio = explorer_num / trainer_gpu_num
+            if opt_ratio_diff > abs(ratio - args.explorer_trainer_ratio):
+                return explorer_num, abs(ratio - args.explorer_trainer_ratio)
+            return opt_explorer_num, opt_ratio_diff
+
         if args.node_num == 1:  # single node
             for trainer_gpu_num in range(1, args.gpu_per_node):
-                if batch_size % trainer_gpu_num == 0:
-                    explorer_gpu_num = args.gpu_per_node - trainer_gpu_num
-                    if explorer_gpu_num % tensor_parallel_size != 0:
-                        continue
-                    explorer_num = explorer_gpu_num // tensor_parallel_size
-                    ratio = explorer_num / trainer_gpu_num
-                    if opt_ratio_diff > abs(ratio - args.explorer_trainer_ratio):
-                        opt_ratio_diff = abs(ratio - args.explorer_trainer_ratio)
-                        opt_explorer_num = explorer_num
+                opt_explorer_num, opt_ratio_diff = update_opt_explorer_num(
+                    trainer_gpu_num, opt_explorer_num, opt_ratio_diff
+                )
         else:  # multi node
             assert (
                 args.gpu_per_node % tensor_parallel_size == 0
-            ), f"Please adjust the value of `tensor_parallel_size` so that it is a divisor of `gpu_per_node`."
+            ), "Please adjust the value of `tensor_parallel_size` so that it is a divisor of `gpu_per_node`."
             for trainer_node_num in range(1, args.node_num):
                 trainer_gpu_num = args.gpu_per_node * trainer_node_num
-                if batch_size % trainer_gpu_num == 0:
-                    explorer_gpu_num = (args.node_num - trainer_node_num) * args.gpu_per_node
-                    explorer_num = explorer_gpu_num // tensor_parallel_size
-                    ratio = explorer_num / trainer_gpu_num
-                    if opt_ratio_diff > abs(ratio - args.explorer_trainer_ratio):
-                        opt_ratio_diff = abs(ratio - args.explorer_trainer_ratio)
-                        opt_explorer_num = explorer_num
+                opt_explorer_num, opt_ratio_diff = update_opt_explorer_num(
+                    trainer_gpu_num, opt_explorer_num, opt_ratio_diff
+                )
         assert (
             opt_explorer_num is not None
-        ), f"Cannot find a suitable explorer number. Please check the value of `train_batch_size`."
+        ), "Cannot find a suitable explorer number. Please check the value of `train_batch_size`."
         config["explorer"]["rollout_model"]["engine_num"] = opt_explorer_num
 
 
-def prepare_configs(args):
+def prepare_configs(args, rank, current_time):
     base_path = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(base_path, "config", f"{args.dataset}-template.yaml")) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
 
-    current_time = time.time()
     current_time_str = time.strftime("%Y%m%d-%H%M%S", time.localtime(current_time))
     run_path = os.path.join(base_path, "runs", current_time_str)
+    if rank != 0:
+        return os.path.join(run_path, "config.yaml")
     os.makedirs(run_path)
+
+    with open(os.path.join(base_path, "config", f"{args.dataset}-template.yaml")) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
     config["name"] += f"-{current_time_str}"
     config["checkpoint_root_dir"] = os.path.join(run_path, "checkpoints")
     set_engine_num(config, args)
-    if args.model_path:
-        model_path = args.model_path
-    elif os.environ.get("MODEL_PATH", None):
-        model_path = os.environ.get("MODEL_PATH")
-    else:
-        model_path = "Qwen/Qwen2.5-1.5B-Instruct"
-    config["model"]["model_path"] = model_path
+    config["model"]["model_path"] = (
+        args.model_path
+        or os.environ.get("MODEL_PATH")
+        or config["model"]["model_path"]
+        or "Qwen/Qwen2.5-1.5B-Instruct"
+    )
+    if ALGORITHM_TYPE.get(config["algorithm"]["algorithm_type"]).use_critic:
+        config["model"]["critic_model_path"] = (
+            args.critic_model_path
+            or config["model"].get("critic_model_path")
+            or config["model"]["model_path"]
+        )
+        if args.critic_lr:
+            config["trainer"]["trainer_config"]["critic"]["optim"]["lr"] = args.critic_lr
+    config["buffer"]["explorer_input"]["taskset"]["path"] = (
+        args.taskset_path
+        or os.environ.get("TASKSET_PATH")
+        or config["buffer"]["explorer_input"]["taskset"]["path"]
+    )
+    assert (
+        config["buffer"]["explorer_input"]["taskset"]["path"] is not None
+    ), "Please specify taskset path."
     if args.lr:
         config["trainer"]["trainer_config"]["actor_rollout_ref"]["actor"]["optim"]["lr"] = args.lr
     if args.sync_interval:
@@ -82,8 +108,30 @@ def prepare_configs(args):
     return config_path
 
 
+def setup_dlc():
+    envs = get_dlc_env_vars()
+    dist.init_process_group(
+        backend="gloo",
+        init_method="env://",
+        world_size=envs["WORLD_SIZE"],
+        rank=envs["RANK"],
+    )
+    if envs["RANK"] == 0:
+        current_time = time.time()
+        time_tensor = torch.tensor([current_time], device="cpu")
+    else:
+        time_tensor = torch.tensor([0.0], device="cpu")
+    dist.broadcast(time_tensor, src=0)
+    dist.destroy_process_group()
+    return envs["RANK"], time_tensor.item()
+
+
 def main(args):
-    config_path = prepare_configs(args)
+    if args.dlc:
+        rank, current_time = setup_dlc()
+    else:
+        rank, current_time = 0, time.time()
+    config_path = prepare_configs(args, rank, current_time)
     cmd_list = [
         "python",
         "-m",
@@ -92,7 +140,7 @@ def main(args):
         "--config",
         config_path,
     ]
-    if args.dlc:  # TODO: bug fix
+    if args.dlc:
         cmd_list.append("--dlc")
     subprocess.run(cmd_list)
 
@@ -125,7 +173,21 @@ if __name__ == "__main__":
         default=None,
         help="Specify the path to the model checkpoint.",
     )
-    parser.add_argument("--lr", type=float, default=None, help="Specify the learning rate.")
+    parser.add_argument(
+        "--critic_model_path",
+        type=str,
+        default=None,
+        help="Specify the path to the critic model checkpoint.",
+    )
+    parser.add_argument(
+        "--taskset_path", type=str, default=None, help="Specify the path to the taskset."
+    )
+    parser.add_argument(
+        "--lr", type=float, default=None, help="Specify the learning rate for actor model."
+    )
+    parser.add_argument(
+        "--critic_lr", type=float, default=None, help="Specify the learning rate for critic model."
+    )
     parser.add_argument(
         "--sync_interval", type=int, default=None, help="Specify the sync interval."
     )
