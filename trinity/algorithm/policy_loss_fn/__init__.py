2c20092aa7364461311440d31ad304fa4cd0694a
问昊 <zwh434786@alibaba-inc.com>
2025-09-01 12:00:26 +0800

    fix precommit

--- trinity/algorithm/policy_loss_fn/__init__.py
+++ trinity/algorithm/policy_loss_fn/__init__.py
@@ -11,8 +11,8 @@ from trinity.algorithm.policy_loss_fn.opmd_policy_loss import OPMDPolicyLossFn
 from trinity.algorithm.policy_loss_fn.policy_loss_fn import POLICY_LOSS_FN, PolicyLossFn
 from trinity.algorithm.policy_loss_fn.ppo_policy_loss import PPOPolicyLossFn
 from trinity.algorithm.policy_loss_fn.sft_loss import SFTLossFn
-from trinity.algorithm.policy_loss_fn.topr_policy_loss import TOPRPolicyLossFn
 from trinity.algorithm.policy_loss_fn.sppo_loss_fn import sPPOPolicyLossFn
+from trinity.algorithm.policy_loss_fn.topr_policy_loss import TOPRPolicyLossFn
 
 __all__ = [
     "POLICY_LOSS_FN",
