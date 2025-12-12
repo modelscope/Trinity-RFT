808e4db8ea7c1c7fd326ed15005e3363eaf0792b
ShenQianli <shenqianli.sql@alibaba-inc.com>
2025-12-12 18:13:46 +0800

    BOTS reference evaluation results collection

---
+++ examples/bots/ref_eval_collect.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+import pandas as pd
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-path", type=str, required=True)
+    parser.add_argument("--ref-eval-path", type=str, required=True)
+    parser.add_argument("--ref-eval-key", type=str, required=True)
+    args = parser.parse_args()
+
+    print(f"Loading original dataset from {args.data_path}...")
+    original_data = pd.read_parquet(args.data_path)
+    prompt2linenum = {}
+    for i, d in enumerate(original_data["prompt"]):
+        prompt2linenum[d[0]["content"]] = i
+    args.ref_eval_path = "./bots_ref_eval_log.jsonl"
+    eval_results = [0. for _ in range(len(original_data))]
+    print(f"Loading reference evaluation results from {args.ref_eval_path}...")
+    print(f"Results will be written to the original dataset at a new column {args.ref_eval_key}...")
+    with open(args.ref_eval_path, "r") as f:
+        for line in f:
+            item = json.loads(line)
+            eval_results[prompt2linenum[item["question"][0]["content"]]] = np.mean(item["rewards"])
+    original_data[args.ref_eval_key] = eval_results
+    print(f"Dataset overwritten at {args.ref_eval_path}...")
+    original_data.to_parquet(args.data_path)
+
+
+if __name__ == "__main__":
+    main()
