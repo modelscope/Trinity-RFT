cf689002627ca1ca394ca3a84c61bec2e36b333f
hiyuchang <sycgloria@gmail.com>
2026-01-21 17:20:48 +0800

    fix pre-commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
-import pytest
 import datetime
+
+import pytest
+
 
 # Get the result of each test
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
@@ -8,20 +10,21 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     setattr(item, "rep_" + rep.when, rep)
 
+
 # Real-time print of start and end of test
 @pytest.fixture(autouse=True)
 def log_test_lifecycle(request):
     node_id = request.node.nodeid
     start_time = datetime.datetime.now().strftime("%H:%M:%S")
-    
+
     print(f"\n[START] {start_time} - Running: {node_id}")
-    
+
     yield
-    
+
     end_time = datetime.datetime.now().strftime("%H:%M:%S")
     # Get the result of each test (setup, call, teardown)
     report = getattr(request.node, "rep_call", None)
-    
+
     if report:
         if report.passed:
             status = "PASSED"
